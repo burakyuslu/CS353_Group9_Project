12c1a46b82d0ba1ae458bee82489db429da903cc
Emre Açıkgöz <emre.acikgoz@ug.bilkent.edu.tr>
2021-04-26 12:54:21 +0300

    initial run

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,29 +1,36 @@
 const usersRouter = require('express').Router()
 const bcrypt = require('bcrypt')
-const User = require('../models/user')
+
+const helper = require('../utils/helper');
+// const config = require('../utils/config');
+const db = require('../services/db')
+
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}).populate('blogs')
-  response.json(users)
+    const rows = await db.query(
+        `SELECT * FROM useracc LIMIT ?,?`, [0, 10]
+    );
+    const data = helper.emptyOrRows(rows);
+    response.json(data)
 })
-usersRouter.post('/', async (request, response) => {
-  const body = request.body
-  const password = body.password
-
-  if (!password || password.length < 3) {
-    return response.status(400).json({ error: 'password length must be at least 3' })
-  }
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
-
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash: passwordHash,
-    blogs: [],
-  })
-
-  const savedUser = await user.save()
-  response.json(savedUser)
-})
+// usersRouter.post('/', async (request, response) => {
+//     const body = request.body
+//     const password = body.password
+//
+//     if (!password || password.length < 3) {
+//         return response.status(400).json({error: 'password length must be at least 3'})
+//     }
+//     const saltRounds = 10
+//     const passwordHash = await bcrypt.hash(password, saltRounds)
+//
+//     const user = new User({
+//         username: body.username,
+//         name: body.name,
+//         passwordHash: passwordHash,
+//         blogs: [],
+//     })
+//
+//     const savedUser = await user.save()
+//     response.json(savedUser)
+// })
 
 module.exports = usersRouter
