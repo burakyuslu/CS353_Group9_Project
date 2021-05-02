bce296846341dcd6bf0dea2b337f3f48b546928a
CanKirsallioba <cankirsallioba@gmail.com>
2021-05-02 21:14:29 +0300

    Merged notes and lecture controller files

--- backend/controllers/peerGrading.js
+++ backend/controllers/peerGrading.js
@@ -11,10 +11,10 @@ peerGradingRouter.post('/', async(request, response) => {
     const assignment_id = body.assignment_id
     const score = body.score
 
-    const rows = await db.query(`SELECT * FROM Submits LEFT OUTER JOIN PeerGrades GROUP BY submission_id HAVING count(review_id) < 3; LIMIT 10; INSERT INTO PeerGrades(student_id, assignment_id, score, review_date) VALUES (?, ?, ?, SYSDATE());`, student_id, assignment_id, score);
+    const peerGrade = await db.query(`SELECT * FROM Submits LEFT OUTER JOIN PeerGrades GROUP BY submission_id HAVING count(review_id) < 3; LIMIT 10; INSERT INTO PeerGrades(student_id, assignment_id, score, review_date) VALUES (?, ?, ?, SYSDATE());`, student_id, assignment_id, score);
     
-    const data = helper.emptyOrRows(rows);
-    response.json(data)
+    const result = helper.emptyOrRows(peerGrade);
+    response.json(result)
 })
 
 module.exports = peerGradingRouter
