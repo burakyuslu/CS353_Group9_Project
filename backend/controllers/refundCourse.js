2606e929f9aa528ed1f4925041dff0817b2edf94
CanKirsallioba <cankirsallioba@gmail.com>
2021-05-04 14:02:49 +0300

    Fixed typo

--- backend/controllers/refundCourse.js
+++ backend/controllers/refundCourse.js
@@ -32,10 +32,12 @@ refundCourseRouter.get('/selectCourseToReturn', async (request, response) => {
         response.status(400).json({error: "You must supply request_id, student_id, admin_id, course_id"})
     }
 
+    //choose the course to be returned and pass its information to the qquery
+
     const selectReturn = await db.query(`
         INSERT INTO RequestRefund(request_id, student_id, admin_id, course_id)
         VALUES (?, ?, ?, ?);
-    ` [request_id, student_id, admin_id, course_id]);
+    `, [request_id, student_id, admin_id, course_id]);
 
     const result = helper.emptyOrRows(selectReturn);
     response.json(result)
