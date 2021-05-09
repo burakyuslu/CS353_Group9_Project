836be1db6094a727cc2fb2ff3954833fcbb7a253
Burak Yiğit Uslu <burakyuslu@gmail.com>
2021-05-09 20:08:48 +0300

    Finished QnA.vue's initial implementation. Needs to be connected to database later.

--- frontend/src/components/QnA.vue
+++ frontend/src/components/QnA.vue
@@ -5,17 +5,17 @@
         Q&A For {{ courseTitle }}
       </h3>
       <v-card v-for="thread in threads" :key="thread.id" outlined>
-        <v-list-item-title class="headline mb-1">
+        <v-card-title class="headline mb-1">
           {{ thread.title }}
-        </v-list-item-title>
-        <v-list-item-subtitle>
+        </v-card-title>
+        <v-card-text>
           by {{ thread.creator }}
-        </v-list-item-subtitle>
+        </v-card-text>
         <v-card-actions>
           <v-btn
               outlined
               text
-              v-on:click="seeThread"
+              v-on:click="seeThread (thread.id)"
           >
             See Thread
           </v-btn>
@@ -31,13 +31,13 @@
     </section>
     <section v-else-if="showPart == 2">
       <h3> Viewing Thread: {{ threadTitle }}</h3>
-      <v-card v-for="entry in threads[selectedThreadId].entries" :key="entry.content" outlined>
-        <v-list-item-title class="headline mb-1">
+      <v-card v-for="entry in threads[selectedThreadIndex].entries" :key="entry.content" outlined>
+        <v-card-title class="headline mb-2">
           {{ entry.poster }} Says:
-        </v-list-item-title>
-        <v-list-item-subtitle>
-          by {{ entry.content }}
-        </v-list-item-subtitle>
+        </v-card-title>
+        <v-card-text>
+          {{ entry.content }}
+        </v-card-text>
       </v-card>
       <v-card>
         <textarea
@@ -50,21 +50,32 @@
         >
           Post Answer
         </v-btn>
+        <v-btn
+            outlined
+            text
+            v-on:click="goBackToThreads"
+        >
+          Go Back
+        </v-btn>
       </v-card>
-
     </section>
     <section v-else>
-      <h3> Create A New Thread {{ threadTitle }}</h3>
-      <p> Enter your thread's title: </p>
-      <input v-model="userEnteredThreadTitle" placeholder="Title"> <input>
-      <p> Give a detailed description of your question: </p>
-      <textarea v-model="message" placeholder="add multiple lines"></textarea>
+      <h3> Create A New Thread In {{ courseTitle }}</h3>
+      <p> Ask your question. This will also be your thread's title: </p>
+      <textarea v-model="userEnteredThreadTitle" placeholder="Your question goes in here..."></textarea>
       <v-btn
           outlined
           text
-          v-on:click="createThread"
+          v-on:click="createNewThread"
       >
         Create New Thread
+      </v-btn>
+      <v-btn
+          outlined
+          text
+          v-on:click="goBackToThreads"
+      >
+        Go Back
       </v-btn>
     </section>
 
@@ -83,14 +94,15 @@ export default {
       threadTitle: "no thread selected",
       userEnteredThreadTitle: "",
       userEnteredAnswer: "",
+      selectedThreadIndex: -1,
       threads: [
         {
-          id: '1',
+          id: '2',
           title: 'How to write better SQL?',
           creator: 'Student User 1',
           entries: [
             {
-              content: "Prompt by student user 1.",
+              content: "Prompt by student user 1, same as the thread title.",
               poster: "Student User 1"
             },
             {
@@ -104,12 +116,12 @@ export default {
           ],
         },
         {
-          id: '2',
-          title: 'Question about triggers',
+          id: '1',
+          title: 'Question about triggers, it will be the same as first entry in final implementation.',
           creator: 'Student User 2',
           entries: [
             {
-              content: "Prompt by student user 2.",
+              content: "Prompt by student user 2, , same as the thread title.",
               poster: "Student User 2"
             },
             {
@@ -125,22 +137,52 @@ export default {
       ],
     };
   },
-  computed: {},
+  computed: {
+
+  },
   methods: {
-    seeThread: function () {
+    seeThread: function (id) {
       console.log("See Thread called.")
       this.showPart = 2;
-      this.selectedThreadId = 1;
+      this.selectedThreadId = id; // is used when posting answer, do not refactor
+      let i;
+      for (i = 0; i < this.threads.length; i++) {
+        if (this.threads[i].id === this.selectedThreadId) {
+          this.selectedThreadIndex = i;
+        }
+      }
+      this.threadTitle = this.threads[this.selectedThreadIndex].title
     },
     postAnswer: function () {
       console.log("Post answer called.");
+      let entryToInsert = {
+          content: this.userEnteredAnswer,
+          poster: 'I DONT KNOW HOW TO RECORD THE USER WHO DID THIS', // todo
+      }
+      this.threads[this.selectedThreadIndex].entries.splice(this.threads[this.selectedThreadIndex].entries.length, 0, entryToInsert);
     },
-    goToCreateNewThread: function(){
+    goToCreateNewThread: function () {
       this.showPart = 3
     },
-    createNewThread: function() {
-      console.log( "Create thread called.");
+    createNewThread: function () {
+      let entryToAdd = {
+        id: this.threads[0].id + 1,
+        title: this.userEnteredThreadTitle,
+        creator: 'I DONT KNOW HOW TO RECORD THE USER WHO DID THIS', // todo
+        entries: [
+          {
+            content: this.userEnteredThreadTitle,
+            poster: 'I DONT KNOW HOW TO RECORD THE USER WHO DID THIS', // todo
+          }
+        ]
+      }
+      this.threads.splice(0, 0, entryToAdd);
+      console.log("Create new thread called.");
       // go back to threads list after creating
+      this.showPart = 1;
+      this.goBackToThreads();
+    },
+    goBackToThreads: function () {
       this.showPart = 1;
     }
   }
