a0f45eb98764a1f9ad59dcfa53187903a8f27b14
CanKirsallioba <cankirsallioba@gmail.com>
2021-05-09 14:26:33 +0300

    Added a prototype course view page

--- frontend/src/views/student/Course.vue
+++ frontend/src/views/student/Course.vue
@@ -1,15 +1,86 @@
 <template>
-  <div>
+  <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col>
+        <v-card
+          class="d-flex"
+          outlined
+          tile
+        >
+          The Complete Web Developer in 2021: Zero to Mastery <br> Learn to code and become a Web Developer in 2021 with HTML, CSS, Javascript, React, Node.js, Machine Learning & more!
+        </v-card>
+      </v-col>
+      <v-col order="12">
+        <v-card
+          class="pa-5"
+          outlined
+          tile
+        >
+          4.7/5.0 <br> (46,766 ratings) <br> 158,605 students
+        </v-card>
+      </v-col>
+      <v-col order="1">
+        <v-card
+          class="pa-5"
+        >
+          The price of the course is $123
+          <br>
+          <v-btn>BUY NOW</v-btn>
+          <br>
+          <v-btn>ADD TO WISHLIST</v-btn>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row no-gutters>
+      <v-col>
+        <v-card
+          class="pa-5"
+          outlined
+          tile
+        >
+          Lectures -LIST- COMPONENT WILL COME
+        </v-card>
+      </v-col>
+      <v-col order="12">
+        <v-card
+          class="pa-5"
+          outlined
+          tile
+        >
+          What you'll learn
+          <br>
+          Skills that will allow you to apply for jobs like: Web Developer, Software Developer, Front End Developer, Javascript Developer, and Full Stack Developer
+          Learn modern technologies that are ACTUALLY being used behind tech companies in 2021
+          Build 10+ real world Web Development projects you can show off
+          Build a professional Portfolio Website
+          Learn best practices to write clean, performant, and bug free code
+          Master modern Web Development fundamentals as well as advanced topics
+          Work as a freelance Web Developer
+          Master beginner and advanced JavaScript topics
+          Learn React + Redux to build rich front end applications
+          Build your own full stack websites and applications
+          Build a complex image recognition app using everything we learn in the course
+          Become a professional Web Developer and get hired
+          Use NodeJS to write server-side JavaScript
+          Learn to implement user authentication
+          Use Express, SQL and PostgreSQL to create fullstack applications that scale
+          Master fundamental concepts in Web Development
+        </v-card>
+      </v-col>
       
-  </div>
+    </v-row>
+
+  </v-container>
 </template>
 
+
 <script>
+
 export default {
-
-}
+  
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-
-</style>
