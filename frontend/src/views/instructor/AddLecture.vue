4520592361d8a35d175946303d29d18d4209fb80
CanKirsallioba <cankirsallioba@gmail.com>
2021-05-11 12:48:29 +0300

    Add lecture module added to instructor

--- frontend/src/views/instructor/AddLecture.vue
+++ frontend/src/views/instructor/AddLecture.vue
@@ -1,5 +1,32 @@
 <template>
-  <v-card> </v-card>
+  <v-form>
+    <v-container >
+      <v-card outlined tile>
+              <v-card-title>
+                Add Your Lecture Material Here
+              </v-card-title>
+      </v-card>
+    </v-container>
+
+    <v-container>
+      <v-text-field>
+        <template v-slot:label>
+          Add the URL of your lecture video here 
+        </template>
+      </v-text-field>
+    </v-container>
+
+    <v-container> 
+      <v-file-input accept="video/*" label="Upload your lecture video here">
+      </v-file-input>
+    </v-container>
+
+    <v-container> 
+      <v-file-input accept=".doc, .txt" label="Upload your lecture text here">
+      </v-file-input>
+    </v-container>
+
+  </v-form>
 </template>
 
 <script>
