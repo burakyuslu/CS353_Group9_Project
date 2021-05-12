2979a0fcec2232da56694726e953d9356a390afe
Emre Açıkgöz <emre.acikgoz@ug.bilkent.edu.tr>
2021-05-12 11:28:00 +0300

    kkk

---
+++ frontend/src/views/auth/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container class="grey lighten-5">
+    <v-card>
+      <v-container>
+        <v-switch :label="text" v-model="value"></v-switch>
+        <v-text-field label="E-mail" v-model="email"></v-text-field>
+        <v-text-field type="password" v-model="password"> </v-text-field>
+      </v-container>
+      <v-card-actions>
+        <v-btn @click="login"> Sign In </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import { actions, mutations } from '../../store/types.js'
+import { mapActions, mapMutations } from 'vuex'
+export default {
+  data() {
+    return {
+      value: false,
+      text: 'Are you an instructor?',
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    ...mapActions({ signIn: actions.SIGN_IN }),
+    ...mapMutations({ setUserType: mutations.SET_USER_TYPE }),
+    login() {
+      this.setUserType(this.userType)
+
+      this.$router.push({ name: this.userType })
+      //   signIn({
+      //     userType: this.userType,
+      //     email: this.email,
+      //     password: this.password,
+      //   })
+    },
+  },
+  computed: {
+    userType() {
+      return this.value ? 'instructor' : 'student'
+    },
+  },
+}
+</script>
+
+<style></style>
