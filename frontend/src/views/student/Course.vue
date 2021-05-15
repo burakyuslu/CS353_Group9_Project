620004b9b8b3a8f8b3557f47b1066ed90dbab40f
Emre Açıkgöz <emre.acikgoz@ug.bilkent.edu.tr>
2021-05-15 20:35:47 +0300

    fix bugs

--- frontend/src/views/student/Course.vue
+++ frontend/src/views/student/Course.vue
@@ -1,192 +1,201 @@
 <template>
-  <v-container class="grey lighten-5">
-    <v-row no-gutters>
-      <v-col>
-        <v-card outlined tile>
-          <pre>
+  <div>
+    <div v-if="loading">
+      Loading
+    </div>
+    <v-container v-else class="grey lighten-5">
+      <v-row no-gutters>
+        <v-col>
+          <v-card outlined tile>
+            <pre>
             {{ courseDetails.course && courseDetails.course.course_name }}
-          </pre>
-        </v-card>
-      </v-col>
-      <v-col order="12">
-        <v-card class="pa-5" outlined tile>
-          {{ courseDetails && courseDetails.avgRating }}/5.0 <br />
-          ({{ courseDetails && courseDetails.ratingCount }} ratings)
-          <br />
-          {{ courseDetails && courseDetails.studentCount }} students
-        </v-card>
-      </v-col>
-      <v-col order="1">
-        <v-card class="pa-5">
-          <div v-if="courses.findIndex(c => c === courseId) < 0">
-            <p>
-              The price of the course is ${{
-                courseDetails.course && courseDetails.course.price
-              }}.
-            </p>
-            <p v-if="isDiscounted">
-              But it is now down to ${{
-                courseDetails.course.price *
-                  ((100 - courseDetails.course.percentage) / 100)
-              }}.
-            </p>
-            <br />
-            <v-dialog
-              v-if="courses && !courses.includes(courseId)"
-              v-model="dialog"
-              persistent
-              max-width="600px"
+          </pre
             >
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn color="primary" dark v-bind="attrs" v-on="on">
-                  Buy Now
-                </v-btn>
-              </template>
-              <v-card>
-                <v-card-title>
-                  <span class="headline">Checkout</span>
-                </v-card-title>
-                <v-card-text v-if="courseDetails.course">
-                  <p>Your Balance: {{ user.balance }}</p>
-                  <p v-if="isDiscounted">
-                    Price: ${{
-                      courseDetails.course.price *
-                        ((100 - courseDetails.course.percentage) / 100)
-                    }}.
-                  </p>
-                  <p v-else>Price: ${{ courseDetails.course.price }}</p>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="dialog = false">
-                    Close
-                  </v-btn>
-                  <v-btn color="blue darken-1" text @click="buyCourse">
-                    Buy
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-            <v-alert class="mt-2" v-model="bought" type="success" dismissible>
-              You have successfully purchased this course.
-            </v-alert>
-            <v-alert v-model="errorOccured" type="error" dismissible>
-              Your balance is not enough.
-            </v-alert>
+          </v-card>
+        </v-col>
+        <v-col order="12">
+          <v-card class="pa-5" outlined tile>
+            {{ courseDetails && courseDetails.avgRating }}/5.0 <br />
+            ({{ courseDetails && courseDetails.ratingCount }} ratings)
             <br />
-            <v-btn
-              v-if="
-                courseDetails.wishlist &&
-                  !courseDetails.wishlist.includes(
-                    courseDetails.course.course_id,
-                  )
+            {{ courseDetails && courseDetails.studentCount }} students
+          </v-card>
+        </v-col>
+        <v-col order="1">
+          <v-card class="pa-5">
+            <div
+              v-if="!courseDetails.studentCourses.includes(Number(courseId))"
+            >
+              <p>
+                The price of the course is ${{
+                  courseDetails.course && courseDetails.course.price
+                }}.
+              </p>
+              <p v-if="isDiscounted">
+                But it is now down to ${{
+                  courseDetails.course.price *
+                    ((100 - courseDetails.course.percentage) / 100)
+                }}.
+              </p>
+              <br />
+              <v-dialog
+                v-if="courses && !courses.includes(courseId)"
+                v-model="dialog"
+                persistent
+                max-width="600px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                    Buy Now
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">Checkout</span>
+                  </v-card-title>
+                  <v-card-text v-if="courseDetails.course">
+                    <p>Your Balance: {{ user.balance }}</p>
+                    <p v-if="isDiscounted">
+                      Price: ${{
+                        courseDetails.course.price *
+                          ((100 - courseDetails.course.percentage) / 100)
+                      }}.
+                    </p>
+                    <p v-else>Price: ${{ courseDetails.course.price }}</p>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="dialog = false">
+                      Close
+                    </v-btn>
+                    <v-btn color="blue darken-1" text @click="buyCourse">
+                      Buy
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+              <v-alert class="mt-2" v-model="bought" type="success" dismissible>
+                You have successfully purchased this course.
+              </v-alert>
+              <v-alert v-model="errorOccured" type="error" dismissible>
+                Your balance is not enough.
+              </v-alert>
+              <br />
+              <v-btn
+                v-if="
+                  courseDetails.wishlist &&
+                    !courseDetails.wishlist.includes(
+                      courseDetails.course.course_id,
+                    )
+                "
+                @click="addToWishlist"
+                >ADD TO WISHLIST</v-btn
+              >
+            </div>
+            <div
+              v-else-if="
+                courseDetails.lectures && courseDetails.lectures.length !== 0
               "
-              @click="addToWishlist"
-              >ADD TO WISHLIST</v-btn
             >
-          </div>
-          <div
-            v-else-if="
-              courseDetails &&
-                courseDetails.lectures &&
-                courseDetails.lectures.length !== 0
-            "
-          >
-            <p>You have this course.</p>
-            <v-btn
+              <p>You have this course.</p>
+              <v-btn
+                link
+                :to="{
+                  name: 'student.lecture',
+                  params: { lectureId: courseDetails.lectures[0].lecture_id },
+                }"
+                color="primary"
+              >
+                Go To The Course
+              </v-btn>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row no-gutters>
+        <v-col>
+          <v-card class="pa-5" outlined tile>
+            <v-card-title>
+              Lectures
+            </v-card-title>
+            <v-card-text
+              v-if="
+                courseDetails &&
+                  courseDetails.lectures &&
+                  courseDetails.lectures.length === 0
+              "
+            >
+              No Lectures Yet!
+            </v-card-text>
+            <v-list-item
+              v-else
+              v-for="item in courseDetails.lectures"
+              :key="item.title"
+              :to="getPath(item)"
               link
-              :to="{
-                name: 'student.lecture',
-                params: { lectureId: courseDetails.lectures[0].lecture_id },
-              }"
-              color="primary"
             >
-              Go To The Course
-            </v-btn>
-          </div>
-        </v-card>
-      </v-col>
-    </v-row>
+              <v-chip
+                v-if="
+                  shouldShowStudentButtons &&
+                    courseDetails.completedLectures &&
+                    courseDetails.completedLectures.includes(item.lecture_id)
+                "
+                class="pa-2 ma-2"
+                color="green"
+                text-color="white"
+              >
+                Done
+              </v-chip>
 
-    <v-row no-gutters>
-      <v-col>
-        <v-card class="pa-5" outlined tile>
-          <v-card-title>
-            Lectures
-          </v-card-title>
-          <v-card-text
-            v-if="
-              courseDetails &&
-                courseDetails.lectures &&
-                courseDetails.lectures.length === 0
-            "
-          >
-            No Lectures Yet!
-          </v-card-text>
-          <v-list-item
-            v-else
-            v-for="item in courseDetails.lectures"
-            :key="item.title"
-            :to="getPath(item)"
-            link
-          >
-            <v-chip
-              v-if="
-                shouldShowStudentButtons &&
-                  courseDetails.completedLectures &&
-                  courseDetails.completedLectures.includes(item.lecture_id)
-              "
-              class="pa-2 ma-2"
-              color="green"
-              text-color="white"
-            >
-              Done
-            </v-chip>
-
-            <v-list-item-content class="text-left">
-              <v-list-item-title>{{ item.lecture_name }} </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
-      </v-col>
-      <v-col order="12">
-        <v-card class="pa-5" outlined tile>
-          <pre>
+              <v-list-item-content class="text-left">
+                <v-list-item-title>{{ item.lecture_name }} </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </v-col>
+        <v-col order="12">
+          <v-card class="pa-5" outlined tile>
+            <pre>
           {{ courseDetails.course && courseDetails.course.course_summary }}
 
-          </pre>
-        </v-card>
-        <v-card outlined tile>
-          <v-card-title primary-title> Ratings </v-card-title>
-          <v-card
-            v-for="rating in courseDetails.ratings"
-            :key="`${rating.studentName}_${rating.comment}`"
-          >
-            <v-rating
-              color="primary"
-              hover
-              length="5"
-              readonly
-              size="32"
-              :value="rating.rating"
-            ></v-rating>
-            <v-card-subtitle>
-              {{ rating.studentName }}
-            </v-card-subtitle>
-            <v-card-text v-if="rating.comment">
-              {{ rating.comment }}
-            </v-card-text>
+          </pre
+            >
           </v-card>
-        </v-card>
-      </v-col>
-    </v-row>
-    <pre>
+          <v-card outlined tile>
+            <v-card-title primary-title> Ratings </v-card-title>
+            <v-card
+              v-for="rating in courseDetails.ratings"
+              :key="`${rating.studentName}_${rating.comment}`"
+            >
+              <v-rating
+                color="primary"
+                hover
+                length="5"
+                readonly
+                size="32"
+                :value="rating.rating"
+              ></v-rating>
+              <v-card-subtitle>
+                {{ rating.studentName }}
+              </v-card-subtitle>
+              <v-card-text v-if="rating.comment">
+                {{ rating.comment }}
+              </v-card-text>
+            </v-card>
+          </v-card>
+        </v-col>
+      </v-row>
+      <pre>
       {{ courseDetails }}
-    </pre>
-    <pre>
+    </pre
+      >
+      <pre>
       {{ courseDetails.studentCourses }}
-    </pre>
-  </v-container>
+    </pre
+      >
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -199,6 +208,7 @@ export default {
     //
     dialog: false,
     assignments: [],
+    loading: true,
     adminData: {
       refundRequests: [],
       discontables: [],
@@ -274,6 +284,7 @@ export default {
     },
     courses() {
       // profile data includes : courses, refundRequests, wishlist, certificates
+      console.log(this.courseDetails.studentCourses)
       return this.courseDetails.studentCourses || []
       // if (this.userProfileData.courses === undefined) return []
       // return courseDetails.courses.map(c => c.course_id)
@@ -292,9 +303,12 @@ export default {
       )
     },
   },
-  async mounted() {
-    this.fetchCourseDetails({ courseId: this.courseId })
-    this.fetchUserProfile()
+  async created() {
+    this.loading = true
+    console.log('here')
+    await this.fetchCourseDetails({ courseId: this.courseId })
+    await this.fetchUserProfile()
+    this.loading = false
   },
   watch: {
     dialog(newValue) {
